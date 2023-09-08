0bb94bccd2ab67d33f33b907e6ca8429827690bf
Samuel E. Sousa <132164905+samueledusousa@users.noreply.github.com>
2023-09-08 08:18:08 -0300

    exemplos da aula 1

---
+++ type-boolean.js
@@ -0,0 +1,15 @@
+// boolean comparison -> ===
+// true -> true
+// false -> false
+
+const primeiroNumero = 5;
+const segundoNumero = 5;
+let cadastroAtivo = true;
+
+//console.log(primeiroNumero === segundoNumero);
+
+
+const texto1 = "Alura"
+const texto2 = "a"
+
+console.log(texto1 === texto2);
---
+++ type-string.js
@@ -0,0 +1,32 @@
+const texto1 = "Hello Word!";
+const texto2 = 'Olá mundo!';
+// const senha = "senhaSuperSegura13499";
+const StringDeNumeros = "0134099"; 
+
+const citacao = "Meu nome é ";
+const meuNome = "bradenburg";
+
+// concatenacao (+)
+
+console.log(citacao + meuNome)
+
+//Para padronizar uma comparação entre strings.
+
+//const cidade = "belo horizonte";
+//const input = "Belo Horizonte";
+
+//console.log(cidade === input); // false
+
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+const inputMinusculo = input.toLowerCase();
+
+console.log(cidade === inputMinusculo); // true
+
+// Propriedade length pode ser utilizada para sabermos quantos caracteres uma string contém.
+
+const senha = "minhasenha1234";
+console.log(senha.length) // 14 caracteres
+
+// Template string OU template literal.
